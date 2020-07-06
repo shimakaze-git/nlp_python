90f2378b2d97db287261b5bbed5e33663e6d899f
shimakaze-git <shimakaze.soft+github@googlemail.com>
2020-07-07 06:50:16 +0900

    refactor: 絶対パスで参照するように修正.

--- wnjpn/wordnet.py
+++ wnjpn/wordnet.py
@@ -1,9 +1,68 @@
-# import sys, sqlite3
+import os
+import sys
 import sqlite3
 from collections import namedtuple
 from pprint import pprint
 
-conn = sqlite3.connect('../wnjpn.db')
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
+conn = sqlite3.connect(path + 'wnjpn.db')
 
 Word = namedtuple('Word', 'wordid lang lemma pron pos')
-print(Word)
+
+
+def getWords(lemma):
+    cur = conn.execute("select * from word where lemma=?", (lemma,))
+    return [Word(*row) for row in cur]
+
+
+Sense = namedtuple('Sense', 'synset wordid lang rank lexid freq src')
+
+
+def getSenses(word):
+    cur = conn.execute("select * from sense where wordid=?", (word.wordid,))
+    return [Sense(*row) for row in cur]
+
+
+Synset = namedtuple('Synset', 'synset pos name src')
+
+
+def getSynset(synset):
+    cur = conn.execute("select * from synset where synset=?", (synset,))
+    return Synset(*cur.fetchone())
+
+
+def getWordsFromSynset(synset, lang):
+    cur = conn.execute("select word.* from sense, word where synset=? and word.lang=? and sense.wordid = word.wordid;", (synset,lang))
+    return [Word(*row) for row in cur]
+
+
+def getWordsFromSenses(sense, lang="jpn"):
+    synonym = {}
+    for s in sense:
+        lemmas = []
+        syns = getWordsFromSynset(s.synset, lang)
+        for sy in syns:
+            lemmas.append(sy.lemma)
+        synonym[getSynset(s.synset).name] = lemmas
+    return synonym
+
+
+def getSynonym(word):
+    synonym = {}
+    words = getWords(word)
+    if words:
+        for w in words:
+            sense = getSenses(w)
+            s = getWordsFromSenses(sense)
+            synonym = dict(list(synonym.items()) + list(s.items()))
+    return synonym
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        synonym = getSynonym(sys.argv[1])
+        pprint(synonym)
+    else:
+        mes = 'You need at least 1 argument as a word like below.\n'
+        mes += 'Example:\n  $ python3 wordnet_jp 楽しい'
+        print(mes)
